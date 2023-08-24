95c6b1a3de72e49c9df23ede64bd9438a0b82686
dimslug <97473046+dimslug@users.noreply.github.com>
2023-08-24 18:49:36 -0400

    Dominic Branch

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Develop Push
+          Learn React - Develop Push - Dominic Branch
         </a>
       </header>
     </div>
