8e9f2e40f26df7802ecde48f0a757a7b89fd51cc
jimjam197 <Sjc97@live.com>
2023-09-19 19:46:04 -0400

    fixed some mild things added admin login

---
+++ Routes/adminRoutes.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const adminAuthMiddleware = require('../Middleware/adminAuthMiddleware');
+
+router.use(adminAuthMiddleware);
+
+
+module.exports = router;
