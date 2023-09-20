32b86b1f4091ff26cfa6708bede01a590ca455e4
jimjam197 <Sjc97@live.com>
2023-09-19 20:43:07 -0400

    formatting

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -1,9 +1,7 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const adminAuthMiddleware = require('../Middleware/adminAuthMiddleware');
+const adminAuthMiddleware = require("../Middleware/adminAuthMiddleware");
 
 router.use(adminAuthMiddleware);
-
 
 module.exports = router;
